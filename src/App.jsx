43ec306686ae22e46a851b2d186adf727d7a6b8f
alwaysAnsh <anshj9y@gamil.com>
2024-01-14 03:09:01 +0530

    installation complete 14Jan

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-800 text-center font-bold ' >Let's create something exciting</div>
+    </>
+  )
+}
+
+export default App
