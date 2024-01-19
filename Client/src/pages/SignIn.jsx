b0aac5f0ae811050ac7018f4527f410bd630ba15
alwaysAnsh <anshj9y@gamil.com>
2024-01-19 23:04:13 +0530

    complete sign in page

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
+
+  const [formData, setFormData ] = useState({})
+  const [loading, setLoading ] = useState(false)
+  const [error, setError ] = useState(null)
+  const navigate = useNavigate();
+
+  const handleOnChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+      [e.target.id] : e.target.value
+      }
+    )
+  }
+  console.log(formData)
+
+  const handleOnSubmit = async(e)=>{
+    setLoading(true)
+    e.preventDefault();
+    const response = await fetch('/api/auth/signin', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    
+      const data = await response.json();
+      console.log(data)
+      setFormData({})
+      setLoading(false)
+      navigate('/')
+    
+  }
+
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto' >
+      <h1 className='text-3xl text-center font-semibold  my-7' >Sign In</h1>
+      <form onSubmit={handleOnSubmit} className='flex flex-col gap-4 '>
+      
+        <input type="email"
+        placeholder='Email'
+        className='border p-3 rounded-lg'
+        id='email'
+        onChange={handleOnChange} />
+
+        <input type="password"
+        placeholder='password'
+        className='border p-3 rounded-lg'
+        id='password'
+        onChange={handleOnChange} />
+
+        <button disabled={loading} type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' >{loading? 'Loading...': 'Sign Up'}</button>
+      </form>
+      <div className='flex gap-2 mt-5' >
+        <p>Do not have an account?</p>
+        <Link to={'/signup'} >
+          <span className='text-blue-700' >Sign Up</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
