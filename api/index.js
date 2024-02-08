4a75a69cb6b187e8345e3bfc76e97718011d3225
alwaysAnsh <anshj9y@gamil.com>
2024-02-08 11:01:15 +0530

    changes in index.js file for hosting

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from '../api/routes/user.routes.js'
 import authRoutes from '../api/routes/auth.routes.js'
 import cookieParser from 'cookie-parser';
 import listingRoutes from '../api/routes/listing.routes.js'
+import path from 'path'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,6 +14,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
 .catch((error)=>{
     console.log("error occured connecting to mongoose - ", error)
 })
+
+
+//this is for hosting the website on render while using vite
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -27,6 +32,12 @@ app.listen(3000, ()=>{
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/listing',listingRoutes)
+
+//you have to write it here only (after the api route) otherwise it is no going to work
+app.use(express.static(path.join(__dirname, '/Client/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Client','dist','index.html'))
+})
 
 //creating a middleware
 app.use((err, req, res, next ) => {
